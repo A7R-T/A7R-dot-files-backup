3c0044c30d5b8171650d3cabd8c248b19e20fe40
A7R <r7omany@gmail.com>
2025-11-11 09:22:23 +0200

    A7R-dotfiles-backup

--- .bashrc
+++ .bashrc
@@ -1,1 +1,13 @@
-/home/a7r/.bashrc
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias qwen='ollama run qwen2.5vl:3b'
+PS1='[\u@\h \W]\$ '
+
+. "$HOME/.local/bin/env"
---
+++ .config/waybar/mediaplayer.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import sys
+import signal
+import gi
+
+gi.require_version('Playerctl', '2.0')
+from gi.repository import Playerctl, GLib
+
+manager = Playerctl.PlayerManager()
+loop = GLib.MainLoop()
+
+def on_play(player, status, manager):
+    on_metadata(player, player.props.metadata, manager)
+
+def on_metadata(player, metadata, manager):
+    track_info = ''
+    if player.props.player_name == 'spotify' and \
+       'mpris:trackid' in metadata.keys() and \
+       ':ad:' in player.props.metadata['mpris:trackid']:
+        track_info = 'AD PLAYING'
+    elif player.get_artist() != '' and player.get_title() != '':
+        track_info = '{artist} - {title}'.format(artist=player.get_artist(),
+                                                  title=player.get_title())
+    else:
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+        return
+
+    if player.props.status == 'Playing':
+        sys.stdout.write(track_info + '\n')
+    else:
+        sys.stdout.write('   ' + track_info + '\n') # Add some spaces if paused/stopped
+    sys.stdout.flush()
+
+def on_name_appeared(manager, name):
+    init_player(name)
+
+def on_player_vanished(manager, player):
+    sys.stdout.write("\n")
+    sys.stdout.flush()
+
+def init_player(name):
+    player = Playerctl.Player.new_from_name(name)
+    player.connect('playback-status', on_play, manager)
+    player.connect('metadata', on_metadata, manager)
+    manager.manage_player(player)
+    on_metadata(player, player.props.metadata, manager)
+
+def signal_handler(sig, frame):
+    loop.quit()
+    sys.stdout.write("\n") # Clear output on exit
+    sys.stdout.flush()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+manager.connect('name-appeared', on_name_appeared)
+manager.connect('player-vanished', on_player_vanished)
+
+for player in manager.props.players:
+    init_player(player.props.player_name)
+
+loop.run()
